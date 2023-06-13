7ed1ab195ac210429f1be0cd9dfdbd8bead0fffe
Ive Herreman <ive.herreman@skyline.be>
2023-06-13 15:21:30 +0200

    Add project files.

---
+++ EmpowerHelloWorld_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmpowerHelloWorld_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("EmpowerHelloWorld_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("3E79EC28-FAD1-41AB-899A-4D1090CDA167")]
+[assembly: AssemblyVersion("1.0.0.0")]
